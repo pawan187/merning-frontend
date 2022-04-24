ca95e49481142cb3c16ce5846d11dcbda7cd33dc
pawan suthar <35306329+pawan187@users.noreply.github.com>
2022-04-24 23:08:33 +0530

    Started with the frontend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import "./styles.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import { AuthProvider } from "./context/auth";
 
 export default function App() {
   return (
